b850ea486feb4acdc6ad4613501ec7c046857442
zph <771036282@qq.com>
2018-12-16 10:22:52 +0800

    删除Impl

--- src/main/java/com/erp/service/IUserService.java
+++ src/main/java/com/erp/service/IUserService.java
@@ -3,12 +3,13 @@ package com.erp.service;
 import com.erp.dao.IUserDao;
 import com.erp.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 /**
  * Created by zph2 on 2018/12/12.
  */
-
+@Service
 public class IUserService{
 
     @Autowired

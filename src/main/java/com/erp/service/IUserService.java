16674ee3807f32a01b8cbc259a6a83bbc9e476dc
zph <771036282@qq.com>
2018-12-16 09:41:37 +0800

    删除Impl

--- src/main/java/com/erp/service/IUserService.java
+++ src/main/java/com/erp/service/IUserService.java
@@ -1,12 +1,23 @@
 package com.erp.service;
 
+import com.erp.dao.IUserDao;
 import com.erp.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 /**
  * Created by zph2 on 2018/12/12.
  */
-public interface IUserService {
+@Service
+public class IUserService implements IUserDao{
 
-    User selectUser(long userId);
+    @Autowired
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    private IUserDao iUserDao;
 
+    @Override
+    public User selectUser(long userId) {
+        return iUserDao.selectUser(userId);
+    }
 }

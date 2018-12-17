4fb475b0684aabc5ce8f37015947a0670f200580
tom_glo <tom_glo@qq.com>
2018-12-17 14:54:30 +0800

    修改web.xml配置、applicationContext.xml、spring-MVC.xml、添加静态资源文件夹 static、错误信息页面 error

--- src/test/com.erp/dao/IUserDaoTest.java
+++ src/test/com.erp/dao/IUserDaoTest.java
@@ -6,11 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 // 加载spring配置文件
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring-mybatis.xml"})
-public class IUserDaoTest {
+@ContextConfiguration({"classpath:applicationContext.xml"})
+class IUserDaoTest {
 
     @Autowired
     @SuppressWarnings("SpringJavaAutowiringInspection")
